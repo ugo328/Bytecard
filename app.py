1db91ea53285b8007d08aa31e39b6653ebe8192e
ugo328 <ugorventura@gmail.com>
2023-10-29 17:33:32 -0300

    90% funcionando

--- app.py
+++ app.py
@@ -1,52 +1,53 @@
-import flask
 from flask import Flask, redirect, render_template, request, flash
 
 import forms
 import use_cases
 from database import db
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.secret_key = b'd3fcbe64-7150-11ee-b1fe-93b6661203c1'
 
 # Configura banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:123456@localhost:3306/byte_card'
 app.config['SQLALCHEMY_ECHO'] = True
 db.init_app(app)
 
-# Configure a chave secreta do Flask
-app.secret_key = 'btg'
-
-# Simulando uma estrutura de dados de usuários
-usuarios = {
-    'ugorventura': {'senha': 'ugodahora'},
-    'olemar': {'senha': 'filho'}
-}
-
 @app.route('/')
 def index():
-    return redirect('/cartoes/lista')
+    return render_template('login.html')
 
-def login():
-    if request.method == 'POST':
-        nickname = request.form['nickname']
-        senha = request.form['senha']
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    # Obtenha as credenciais do formulário
+    nickname = request.form['apelido']
+    senha = request.form['senha']
 
-        if nickname in usuarios and usuarios[nickname]['senha'] == senha:
-            session['logged_in'] = True
-            flash('Login bem-sucedido!')
-            return redirect(url_for('index'))
-        else:
-            flash('Credenciais inválidas. Tente novamente.')
+    # Verifique as credenciais (substitua por sua lógica de autenticação real)
+    if nickname == 'ugorventura' and senha == 'ugodahora':
+        # Se as credenciais estiverem corretas, redirecione para a página de cartões
+        return redirect('/cartoes/lista')
+    else:
+        # Se as credenciais estiverem incorretas, exiba uma mensagem de erro na tela de login
+        flash('Credenciais inválidas. Tente novamente.')
 
     return render_template('login.html')
 
+@app.route('/cartoes/lista')
+def lista_cartoes():
+    # Verifique se o usuário está autenticado (você pode adicionar essa lógica)
+    return render_template('cartao/lista.html', cartoes=use_cases.lista_cartoes())
 
-# Rota para a página de logout
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('Logout bem-sucedido!')
-    return redirect(url_for('index'))
-    return render_template('login.html')
+
+
+
+
+
+@app.route('/cartoes/formulario')
+def formulario_cartao(form=None):
+    return render_template('cartao/formulario.html', form=form)
+
 
 @app.route('/cartoes/cadastrar', methods=['POST'])
 def cadastra_cartao():
@@ -54,7 +55,9 @@ def cadastra_cartao():
     if form.validate():
         use_cases.cadastra_cartao(form.cliente.data, form.limite.data)
         flash('Cartão cadastrado com sucesso.', 'info')
+
         return redirect('/cartoes/lista')
+
     return formulario_cartao(form)
 
 
